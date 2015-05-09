3406c2a5ab3b654709b1961bc0aaa1904bb7c1a1
glennpackerBV <glenn.packer@bevalued.co.uk>
2015-05-09 10:21:14 +0100

    initial

---
+++ src/Api.Services/FooFactory.cs
@@ -0,0 +1,32 @@
+﻿using Api.Core;
+using Api.Domain;
+using ApiProxy.Models;
+
+namespace Api.Services
+{
+    public class FooFactory : IFooFactory
+    {
+        public Foo Create(ApiFoo apiFoo)
+        {
+            var result = new Foo
+            {
+                Description = apiFoo.Description,
+                Name = apiFoo.Name,
+                Id = apiFoo.Id
+            };
+            return result;
+        }
+
+        public ApiFoo Create(Foo foo)
+        {
+            var result = new ApiFoo
+            {
+                Description = foo.Description,
+                Name = foo.Name,
+                Id = foo.Id
+            };
+            return result;
+        }
+
+    }
+}
---
+++ src/ApiProxy/Models/ApiFoo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiProxy.Models
+{
+    public class ApiFoo
+    {
+        public int Id { get; set; }
+
+        [StringLength(255, ErrorMessage = "*")]
+        public string Name { get; set; }
+
+        [StringLength(255, ErrorMessage = "*")]
+        [Required]
+        public string Description { get; set; }
+    }
+}
